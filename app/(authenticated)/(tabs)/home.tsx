730b8a3a7b1f22705481feda3bf9499d9618db2e
danhnc <danhnc1302@gmail.com>
2024-06-13 23:49:39 +0700

    something wrong with my homescreen :(((

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,9 +1,95 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useHeaderHeight } from '@react-navigation/elements';
+import Dropdown from '../../../components/Dropdown';
+import RoundBtn from '../../../components/RoundBtn';
+import WidgetList from '../../../components/SortableList/WidgetList';
+import Colors from '../../../constants/Colors';
+import { defaultStyles } from '../../../constants/Styles';
+import { useBalanceStore } from '../../../store/balanceStore';
+
 const Page = () => {
+  const headerHeight = useHeaderHeight();
+
+  const onAddMoney = () => {
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{
+        paddingTop: headerHeight,
+      }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{}</Text>
+          <Text style={styles.currency}>€</Text>
+        </View>
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButtonSmall,
+            { backgroundColor: Colors.lightGray, marginVertical: 20 },
+          ]}>
+          <Text style={[defaultStyles.buttonTextSmall, { color: Colors.dark }]}>Accounts</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* <View style={styles.actionRow}>
+        <RoundBtn icon={'add'} text={'Add money'} onPress={onAddMoney} />
+        <RoundBtn icon={'refresh'} text={'Exchange'} onPress={ () => {}} />
+        <RoundBtn icon={'list'} text={'Details'} /> 
+        <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        
+      </View>
+      <Text style={defaultStyles.sectionHeader}>Widgets</Text>
+      <WidgetList />  */}
+    </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  actionRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    gap: 20,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default Page;
+
+
