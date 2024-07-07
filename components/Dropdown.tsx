624e503f2f3382fa7f7580f7477e6d1035b749c5
danhnc1302 <danhnc1302@gmail.com>
2024-07-07 23:37:29 +0700

    Comeback and complete popupmenu

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,5 @@
 import RoundBtn from '@/components/RoundBtn';
+import { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import {
   MenuContext,
@@ -8,23 +9,40 @@ import {
   MenuTrigger,
   MenuProvider,
 } from 'react-native-popup-menu';
+import Colors from '@/constants/Colors';
+import { Foundation, FontAwesome6, Entypo, AntDesign } from '@expo/vector-icons';
 
 const Dropdown = () => {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const togglePopUpMenu = () => {
+    setIsOpened(!isOpened)
+  }
+
   return (
-    <Menu opened={true}>
+    <Menu opened={isOpened} onBackdropPress={togglePopUpMenu}>
       <MenuTrigger style={styles.button}>
-        <RoundBtn icon={'ellipsis-horizontal'} text={'More'} />
+        <RoundBtn onPress={togglePopUpMenu} icon={'ellipsis-horizontal'} text={'More'} />
       </MenuTrigger>
-      <MenuOptions>
-        <MenuOption onSelect={() => alert(` You clicked on Save`)} text="Save" />
-        <MenuOption onSelect={() => alert(`You Clicked on Delete`)}>
-          <Text style={{ color: 'red' }}>Delete</Text>
+      <MenuOptions customStyles={{
+        optionsWrapper: styles.wrapper
+      }}>
+        <MenuOption onSelect={() => alert(` You clicked on Save`)} style={styles.optionContainer} >
+          <Text>Statement</Text>
+          <Foundation name="list-bullet" size={24} color={Colors.dark} />
         </MenuOption>
-        <MenuOption
-          onSelect={() => alert(`Not called`)}
-          disabled={true}
-          text="Disabled"
-        />
+        <MenuOption onSelect={() => alert(`You Clicked on Delete`)} style={styles.optionContainer}>
+          <Text>Converter  </Text>
+          <FontAwesome6 name="arrows-rotate" size={24} color={Colors.dark} />
+        </MenuOption>
+        <MenuOption onSelect={() => alert(`You Clicked on Delete`)} style={styles.optionContainer}>
+          <Text>Background</Text>
+          <Entypo name="images" size={24} color={Colors.dark} />
+        </MenuOption>
+        <MenuOption onSelect={() => alert(`You Clicked on Delete`)} style={styles.optionContainer}>
+          <Text>Add new account</Text>
+          <AntDesign name="addfolder" size={24} color={Colors.dark} />
+        </MenuOption>
       </MenuOptions>
     </Menu>
   );
@@ -39,6 +57,21 @@ const styles = StyleSheet.create({
   button: {
     textAlign: 'center',
     display: 'flex',
+  },
+  optionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 20,
+    borderBottomWidth: 0.5,
+    borderColor: Colors.lightGray
+  },
+  wrapper: {
+    position: "absolute",
+    padding: 10,
+    right: 55,
+    bottom: -20,
+    borderRadius: 10,
+    backgroundColor: "white"
   }
 });
 
